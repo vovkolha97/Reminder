62f0b8ef9f22120a8c33a2b4829309989d895f50
Olha Vovk <vovkolha97@gmail.com>
2019-12-26 14:57:14 +0200

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Вовк Ольга' on 26.12.2019.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
